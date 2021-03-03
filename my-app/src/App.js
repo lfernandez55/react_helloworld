66b94d37291e12a0b21c244e34389b250705f69a
lfernandez55 <lfernandez@weber.edu>
2021-03-03 16:35:51 -0700

    Added some simple routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import Main from './components/Main.js'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Nav Links Here</p>
-      </header>
-      <Main />
-      <footer className="App-footer">
-        <p>Copyright 2021</p> 
-        <p>Environment Variable: {process.env.REACT_APP_SERVER_URL}</p> 
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/list">List</Link></li>
+            <li><Link to="/about">About</Link></li>
+         </ul>
+          <Route path="/list">
+            <button className="Sort-button">Sort List</button>
+          </Route>
+        </header>
+        <Main />
+        <footer className="App-footer">
+          <p>Copyright 2021</p> 
+          <p>Environment Variable (set in root/.env): {process.env.REACT_APP_SERVER_URL}</p> 
+        </footer>
+      </div>
+    </Router>
   );
 }
 
--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,7 +1,21 @@
+import {Switch, Route, Link, Redirect, useHistory} from 'react-router-dom'
+
 export default function Main() {
     return (
         <div>
-            <h2>In Main Component</h2>
+            
+            <Switch>
+
+                <Route path="/list">
+                    <h4>Build a list component here</h4>
+                </Route>
+                <Route path="/about">
+                    <h4>Build an about component here</h4>
+                </Route>
+                <Route path="/">
+                    <h4>Default Home Content</h4>
+                </Route>
+            </Switch>
         </div>
     )
 }

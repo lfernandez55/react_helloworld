281df4dd5519967b8367f295d4a6b39c6afda549
lfernandez55 <lfernandez@weber.edu>
2021-03-03 19:37:02 -0700

    Serves up front end on 8080 after installing express and >npm run build
    Doing >npm run build creates a build folder and server.js also has it's static folder mapped to that folder.  So the server is now serving the frontend.

---
+++ my-app/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+
+// var cors = require('cors')
+// app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+
+app.get('/api/test', function (req, res) {
+    let msg = {};
+    msg.sender="Joe";
+    msg.writing="Roses are red....";
+    return res.send(JSON.stringify(msg));
+});
+
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+app.listen(process.env.PORT || 8080);

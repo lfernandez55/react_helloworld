a75058d9120ce3fe75ca86aeb3e6bd587e296eb0
lfernandez55 <lfernandez@weber.edu>
2022-04-19 23:38:59 -0600

    Refactored ProjectForm and ProjectList

--- my-app/src/components/ProjectForm.js
+++ my-app/src/components/ProjectForm.js
@@ -39,9 +39,7 @@ export default function ProjectForm() {
 
 
     const addProjForm = (e) => {
-        console.log("in addProjForm" + e)
         e.preventDefault();
-        console.log(project)
         let newProjs;
         if (pid) {
             //update
@@ -55,73 +53,9 @@ export default function ProjectForm() {
             //add
             newProjs = [...projects];
             newProjs.push(project)
-            console.log("hohoohoohh")
-            console.log(newProjs)
         }
         setProjects([...newProjs])
         navigate('/list')
-
-        // console.log(e.target.id.value)
-        // let formObj = {};
-        // formObj.id = e.target.id.value;
-        // formObj.title = e.target.title.value;
-        // formObj.description = e.target.description.value;
-        // console.log(formObj)
-
-        // let url = ""
-        // let fetch_method = ""
-        // if (pid) {
-        //     url = "api/projects/" + pid;
-        //     fetch_method = "PUT";
-        // } else {
-        //     url = "api/projects/";
-        //     fetch_method = "POST";
-        // }
-
-        // fetch(url, {
-        //     method: fetch_method,
-        //     headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-        //     body: JSON.stringify(formObj),
-        //     credentials: "include"
-        // })
-        //     .then((response) => {
-        //         return response.json();
-        //     })
-        //     .then((resp) => {
-        //         // the add was successful on the backend so update the context
-        //         /*
-        //         The below updates to state aren't needed becuz when you redirect to 
-        //         ProjectList useEffect reloads the projects from the db and updates the state
-
-        //         if(resp.method === "POST"){
-        //             console.log("new", resp._id, project)
-        //             // create a new project and 
-        //             project._id = resp._id;
-        //             projects.push(project)
-        //             setProjects([...projects])
-        //         } else {
-        //             console.log("update", resp._id, project)
-        //             // create a new proj array, only instead of the original project use the updated project
-        //             // or in greater detail:
-        //             // using map go thru each project in the original projects array
-        //             // using the _id, if the project is one we want to update, replace it with the new one
-        //             let newProjs = projects.map(e => {
-        //                 if (e.id == resp._id){
-        //                   e = project
-        //                 }
-        //                 return e;
-        //             })
-        //             // re-update the projects state 
-        //             setProjects([...newProjs])
-        //         }
-        //         */
-        //         setDBUpdated(true)
-        //         navigate('/list')
-        //     })
-        //     .catch((err) => {
-        //         // Code called when an error occurs during the request
-        //         console.log(err.message);
-        //     });
 
     }
 
--- my-app/src/components/ProjectList.js
+++ my-app/src/components/ProjectList.js
@@ -3,16 +3,13 @@ import { ProjectContext } from '../App.js';
 import { useNavigate, Link } from 'react-router-dom'
 
 export default function ProjectList(props) {
-    // let { setDBUpdated } = useContext(ProjectContext)
     let { projects, setProjects } = useContext(ProjectContext)
 
     const navigate = useNavigate()
 
 
     const deleteMe = (param) => {
-        console.log("in deleteMe", param)
         let newProjArray = projects.filter(proj => {
-            console.log(proj.id + ":" + param)
             if (proj.id !== param) {
                 return true
             } else {
@@ -21,36 +18,8 @@ export default function ProjectList(props) {
 
         }
         )
-        console.log(newProjArray)
         setProjects(newProjArray)
     }
-
-    // const deleteMe = (param) => {
-    //     let url = "api/projects/" + param;
-    //     fetch(url, {
-    //         method: "DELETE",
-    //     })
-    //         .then((response) => {
-    //             return response.json();
-    //         })
-    //         .then((resp) => {
-    //             // the delete was successful on the backend so update the context
-
-    //             /* Instead of running these two line:
-    //                 let newProjArray = projects.filter(proj => proj._id !== param)
-    //                 setProjects(newProjArray)
-    //                I simply change the setDBUpdated.  Over in App.js the useEffect is
-    //                watching for a change to this var.  If it changes, it re-queries the
-    //                db which also updates the state.
-    //             */
-    //             setDBUpdated("changed")
-    //         })
-    //         .catch((err) => {
-    //             // Code called when an error occurs during the request
-    //             console.log(err.message);
-    //         });
-    // }
-
 
     return (
         <div>

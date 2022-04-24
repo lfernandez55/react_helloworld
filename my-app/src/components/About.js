92ce1cb90ae4fbe50036d991aff0d988898e0311
lfernandez55 <lfernandez@weber.edu>
2022-04-24 15:59:36 -0600

    Got rid of src folder, stuff in package.json

--- my-app/src/components/About.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-    return (
-        <div>
-            <h1>An About Page</h1>
-            <p>Nothing to see here folks. . .</p>
-        </div>
-    )
-}
--- my-app/src/components/Main.js
+++
@@ -1,8 +0,0 @@
-export default function Main() {
-    return (
-        <div>
-            <h1>Projects Site</h1>
-            <p>Practice Your CRUD Methods. . . .</p>
-        </div>
-    )
-}
